a80e1ddb866fdc7db940833296ed9e37e660908a
zainab hodroj <92784831+zainabhodroj@users.noreply.github.com>
2022-05-21 08:28:27 +0300

    Update Datavis.py

--- Datavis.py
+++ Datavis.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly_express as px
 showWarningOnDirectExecution = False
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
