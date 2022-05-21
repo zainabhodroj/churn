e4749f73dc0cae57bea2be56528a3c7ba45dd11f
zainab hodroj <92784831+zainabhodroj@users.noreply.github.com>
2022-05-21 11:50:58 +0300

    Update Modelapp.py

--- Modelapp.py
+++ Modelapp.py
@@ -11,27 +11,6 @@ st.set_page_config(
     page_title="Customer Churn Predection App",
     layout="wide",
     initial_sidebar_state="expanded")
-st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #faebd7;">
-  <a class="Dataset" href="https://www.kaggle.com/datasets/blastchar/telco-customer-churn" target="_blank">Dataset</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-        <li class="nav-item">
-        <a class="nav-link" href="https://share.streamlit.io/zainabhodroj/churn/main/home.py" target="_blank">Overview</a>
-      </li>
-      <li class="nav-item"> 
-        <a class="nav-link" href="https://share.streamlit.io/zainabhodroj/datavis/main/Datavis.py" target="_blank">DataVisulization</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="https://share.streamlit.io/zainabhodroj/churn/main/Modelapp.py" target="_blank">Model Prediction</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-""", unsafe_allow_html=True)
 
 # Title for the app
 image = Image.open('customer.png')
