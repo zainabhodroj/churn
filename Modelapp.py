5b0fbc8436947449ed084bcf7d45f0b019a3dfd8
zainab hodroj <92784831+zainabhodroj@users.noreply.github.com>
2022-05-21 15:56:58 +0300

    Update Modelapp.py

--- Modelapp.py
+++ Modelapp.py
@@ -20,9 +20,6 @@ st.markdown("""
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link disabled" href="#">Home <span class="sr-only"></span></a>
-      </li>
         <li class="nav-item">
         <a class="nav-link" href="https://share.streamlit.io/zainabhodroj/sthome/main/home.py" target="_blank">Overview</a>
       </li>
